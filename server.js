03763d463347d61a38bd4a054f2a2d50eb663ee6
Luan <oliveiraluan03@gmail.com>
2022-07-04 17:20:04 -0300

    a msg smp

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const MongoStore = require('connect-mongo'); // salvar sessoes no banco de dados
 const flash = require('connect-flash'); // mensagem que somen
 const routes = require('./routes'); 
 const path = require('path');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 const csrf = require('csurf'); 
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-app.use(helmet());
+//app.use(helmet());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
